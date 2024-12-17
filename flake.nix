bb1afc7c74dd2f451467450b46ebe910fc32482d
whatever <whtvr@narod.ru>
2024-12-17 13:41:53 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "whatever Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    brlaser_nixpkgs.url = "github:ahydronous/nixpkgs/e4b52e42c6f6ededdf64a412393fc3d6bd681555";
+
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, brlaser_nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        hosaka = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager = {
+                users.whatever = import ./home.nix;
+                useGlobalPkgs = true;
+                useUserPackages = true;
+              };
+            }
+            nixos-hardware.nixosModules.lenovo-thinkpad-t460s
+            { 
+              _module.args = {
+                inherit brlaser_nixpkgs;
+              };
+            }
+
+          ];
+        };
+      };
+  };
+}
