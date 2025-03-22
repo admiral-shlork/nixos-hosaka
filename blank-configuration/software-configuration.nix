e579109d5ad98a8057ec24c16af93606726a4f5a
whatever <vvhtvr@protonmail.ch>
2025-03-22 18:55:15 +0000

    switching to KDE

--- blank-configuration/software-configuration.nix
+++ blank-configuration/software-configuration.nix
@@ -7,11 +7,15 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Enable the KDE Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
+  qt = {
+    enable = true;
+    platformTheme = "gnome";
+    style = "adwaita-dark";
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -37,18 +41,18 @@
     git
     dconf-editor
     ghostty
-    gnomeExtensions.appindicator
-    gnomeExtensions.caffeine
-    gnomeExtensions.dash-to-panel
-    gnomeExtensions.date-menu-formatter
-    gnomeExtensions.night-theme-switcher
-    gnomeExtensions.no-overview
     home-manager
     nh
     terminator
     vim
     vscodium
     wget
+  ];
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    # plasma-browser-integration
+    # konsole
+    # oxygen
   ];
 
   fonts.packages = with pkgs; [
@@ -67,36 +71,6 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Minimal gnome installation
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      atomix
-      cheese
-      epiphany
-      evince
-      geary
-      gedit
-      gnome-calendar
-      gnome-characters
-      gnome-connections
-      gnome-console
-      gnome-contacts
-      gnome-maps
-      gnome-music
-      gnome-photos
-      gnome-software
-      gnome-terminal
-      gnome-tour
-      gnome-weather
-      hitori
-      iagno
-      simple-scan
-      snapshot
-      tali
-      totem
-      yelp
-    ]);
 
   programs.steam = {
     enable = true;
