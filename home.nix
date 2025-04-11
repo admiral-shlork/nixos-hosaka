8368c4b8999c31af1967e41780e420b5b6974e33
whatever <vvhtvr@protonmail.ch>
2025-04-11 12:38:30 +0100

    installing Arc browser

--- home.nix
+++ home.nix
@@ -12,6 +12,7 @@
     homeDirectory = "/home/whatever";
     stateVersion = "24.11";
     packages = with pkgs; [
+      arc-browser
       # audacity
       # calibre
       # deadbeef-with-plugins
