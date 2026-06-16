cf637c5c3893b418ce53b16b66d6b7929aab8a72
admiral-shlork <vvhtvr@protonmail.ch>
2026-06-16 13:48:50 +0100

    more refactoring

--- blank-configuration/hardware-configuration.nix
+++ blank-configuration/hardware-configuration.nix
@@ -2,36 +2,32 @@
 
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # virtualisation.virtualbox.guest.enable = true;
 
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  # wifi
-  hardware.enableAllFirmware = true;
-  hardware.firmware = [ pkgs.linux-firmware ];
-
-  # bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        UserspaceHID = true;
-        ClassicBondedOnly = true;
-      };
-      Policy = {
-        AutoEnable = true;
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    enableAllFirmware = true;
+    firmware = [ pkgs.linux-firmware ];
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          UserspaceHID = true;
+          ClassicBondedOnly = true;
+        };
+        Policy = {
+          AutoEnable = true;
+        };
       };
     };
   };
+
   services.blueman.enable = true;
 
   services.hardware.bolt.enable = false;
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -39,12 +35,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
--- blank-configuration/storage-configuration.nix
+++ blank-configuration/storage-configuration.nix
@@ -1,29 +1,30 @@
 { config, pkgs, modulesPath, ... }:
 
 {
-boot = {
-  initrd.availableKernelModules = [ "ahci" "ata_piix" "ohci_pci" "ehci_pci" "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-  initrd.kernelModules = [ "dm-snapshot" ];
-  initrd.systemd.enable = true;
-  kernelModules = [ "kvm-amd" "8821ce" ];
-  kernelPackages = pkgs.linuxPackages_latest;
-  kernelParams = [ "usbocre.autosuspend=-1" "amdgpu.reset=1"];
-  extraModulePackages = [ ];
-  loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  initrd.luks.devices = let
-    luks_root_uuid = "881898be-007b-45d1-96cd-206e5e360beb";
-  in {
-    # LUKS container with root partition
-    "luks-${luks_root_uuid}" = {
-      device = "/dev/disk/by-uuid/${luks_root_uuid}";
-      allowDiscards = true;
+  boot = {
+    initrd = {
+      availableKernelModules = [ "ahci" "ata_piix" "ohci_pci" "ehci_pci" "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+      kernelModules = [ "dm-snapshot" ];
+      systemd.enable = true;
+      luks.devices = let
+        luks_root_uuid = "881898be-007b-45d1-96cd-206e5e360beb";
+      in {
+        # LUKS container with root partition
+        "luks-${luks_root_uuid}" = {
+          device = "/dev/disk/by-uuid/${luks_root_uuid}";
+          allowDiscards = true;
+        };
+      };
+    };
+    kernelModules = [ "kvm-amd" "8821ce" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "usbocre.autosuspend=-1" "amdgpu.reset=1"];
+    extraModulePackages = [ ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
   };
-};
 
   swapDevices = [{
       device = "/swapfile";
@@ -35,7 +36,7 @@ boot = {
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
-  
+
   fileSystems."/" =
     { device = "/dev/disk/by-label/root";
       fsType = "ext4";
